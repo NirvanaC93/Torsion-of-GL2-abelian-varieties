dda3c12a35f9e3adf5df74212440568f4b7a6364
NirvanaC93 <58878663+NirvanaC93@users.noreply.github.com>
2026-02-22 20:46:02 +0100

    functions AddDivisors and SortList added

---
+++ AddDivisors.m
@@ -0,0 +1,18 @@
+//Two useful functions to state the correct conjectures taking into account the divisors of the numbers in the lists computed in PossibleTorsionOrders.m. The first one adds the divisors of the numbers in the list, and the second one sorts the list and removes duplicates.
+
+function AddDivisors(L)
+    for l in L do
+        for d in Divisors(l) do
+            if d notin L then
+                L := Append(L, d);
+            end if;
+        end for;
+    end for;
+    return L;
+end function;
+
+function SortList(L)
+    L := Setseq(Set(L));
+    Sort(~L);
+    return L;
+end function;
