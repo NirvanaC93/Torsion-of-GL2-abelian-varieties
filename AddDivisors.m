9ff32617c795499f1f27e1a9e85ff48d66080846
NirvanaC93 <58878663+NirvanaC93@users.noreply.github.com>
2026-02-22 21:06:54 +0100

    sortedlists added; todo: finish the g4 case

--- AddDivisors.m
+++ AddDivisors.m
@@ -1,4 +1,4 @@
-//Two useful functions to state the correct conjectures taking into account the divisors of the numbers in the lists computed in PossibleTorsionOrders.m. The first one adds the divisors of the numbers in the list, and the second one sorts the list and removes duplicates.
+//A useful function to state the correct conjectures taking into account the divisors of the numbers in the lists computed in PossibleTorsionOrders.m. The first one adds the divisors of the numbers in the list, and the second one sorts the list and removes duplicates.
 
 function AddDivisors(L)
     for l in L do
